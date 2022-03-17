e61d8e93445e4aa26442faca46f632112041fd0f
Markus Kainu srv1 <markus.kainu@kela.fi>
2022-03-17 17:05:38 +0200

    Uudet paikalliset datat, sovellus valmis testijulkaisuun

---
+++ data-raw/data_viikko.R
@@ -0,0 +1,14 @@
+## code to prepare `data_viikko` dataset goes here
+
+data_viikko <- dplyr::bind_rows(
+  readr::read_csv2('https://github.com/kelaresearchandanalytics/korona_atc_data/raw/master/data_viikko_2019.csv'),
+  readr::read_csv2('https://github.com/kelaresearchandanalytics/korona_atc_data/raw/master/data_viikko_2020.csv'),
+  readr::read_csv2('https://github.com/kelaresearchandanalytics/korona_atc_data/raw/master/data_viikko_2021.csv'),
+  readr::read_csv2('https://github.com/kelaresearchandanalytics/korona_atc_data/raw/master/data_viikko_2022.csv')
+)
+
+usethis::use_data(data_viikko, overwrite = TRUE)
+# document
+karttasovellus::document_data(dat = data_viikko, 
+                              neim = "data_viikko", 
+                              description = paste("Githubista", Sys.Date(), "haettu data"))
---
+++ data-raw/metadata_viikko.R
@@ -0,0 +1,9 @@
+## code to prepare `metadata_viikko` dataset goes here
+
+metadata_viikko <- readr::read_csv2('https://github.com/kelaresearchandanalytics/korona_atc_data/raw/master/metadata_viikko.csv')
+usethis::use_data(metadata_viikko, overwrite = TRUE)
+
+# document
+karttasovellus::document_data(dat = metadata_viikko, 
+                              neim = "metadata_viikko", 
+                              description = paste("Githubista", Sys.Date(), "haettu metadata"))
