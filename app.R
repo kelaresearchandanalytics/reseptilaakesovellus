db96516264607946854b72849937f9753ad3e0c7
Markus Kainu <markus.kainu@kela.fi>
2022-03-31 22:41:16 +0300

    openshiftdeployent testi

--- app.R
+++ app.R
@@ -4,4 +4,7 @@
 
 pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
 options( "golem.app.prod" = TRUE)
+options(sass.cache = FALSE)
+options( "shiny.useragg" = TRUE)
+
 reseptilaakesovellus::run_app() # add parameters here (if any)
