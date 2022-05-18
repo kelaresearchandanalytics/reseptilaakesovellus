3ae206e78f1b4fa28ee7c8ee3e5bf499209d1da7
Markus Kainu srv1 <markus.kainu@kela.fi>
2022-05-18 09:59:59 +0300

    .gitignore toimii taas

--- inst/deploy/test_locally.sh
+++
@@ -1,7 +0,0 @@
-IMAGENAME=reseptilaakesovellus
-docker build -t $IMAGENAME .
-# testaa ensin interaktiivisesti R:ssä esim. ajamalla < R -e "shiny::runApp()" >
-docker run -ti --rm reseptilaakesovellus bash
-# docker run --env-file .Renviron $IMAGENAME
-#docker run $IMAGENAME
-docker run --rm -p 3840:80 reseptilaakesovellus
